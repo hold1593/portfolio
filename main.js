15e83f323c5e36bb91a6642a1edcd3fa5371d683
mimi <hold1593@naver.com>
2021-01-28 18:00:27 +0900

    js : home 투명도

--- main.js
+++ main.js
@@ -3,19 +3,13 @@
 // Navbar (스크롤에 따라 위로는 투명 아래로는 색 있도록)
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener('scroll', () =>{
+document.addEventListener('scroll', () => {
   if(window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   }else{
     navbar.classList.remove('navbar--dark');
   }
 });
-
-// scroll 이벤트 함수 정의
-function scrollIntoView(selector) {
-  const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({behavior: 'smooth'});
-}
 
 //  Navbar의 탭 해당구간 연결
 const navbarMenu = document.querySelector('.navbar__menu');
@@ -33,3 +27,16 @@ const contactBtn = document.querySelector('.home__contact');
 contactBtn.addEventListener('click', () => {
   scrollIntoView('#contact');
 });
+
+// 스크롤링 시 home 투명하게
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// scroll 이벤트 함수 정의
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({behavior: 'smooth'});
+}
